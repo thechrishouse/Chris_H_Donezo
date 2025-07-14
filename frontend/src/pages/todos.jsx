2b9fa8791b0f3976f34e032e4c7796d986d29636
Christopher House <chrishouse180@gmail.com>
2025-07-14 12:59:42 -0700

    included queryClient into todo.jsx file

--- frontend/src/pages/todos.jsx
+++ frontend/src/pages/todos.jsx
@@ -5,21 +5,52 @@ import getAxiosClient from "../axios-instance";
 
 export default function Todos() {
   const modalRef = useRef();
+  const queryClient = useQueryClient();
 
   const { mutate: createNewTodo } = useMutation({
-	  // The key used to identify this mutation in React Query's cache
-	  mutationKey: ["newTodo"],
-	
-	  // The function that performs the mutation (i.e., creating a new to-do)
-	  mutationFn: async (newTodo) => {
-	    const axiosInstance = await getAxiosClient();
-	
-	    // Use the Axios instance to make a POST request to the server, sending the new to-do data
-	    const { data } = await axiosInstance.post("http://localhost:8080/todos", newTodo);
-	
-	    // Return the response data (e.g., the newly created to-do object)
-	    return data;
-	  },
+    // The key used to identify this mutation in React Query's cache
+    mutationKey: ["newTodo"],
+
+    // The function that performs the mutation (i.e., creating a new to-do)
+    mutationFn: async (newTodo) => {
+      const axiosInstance = await getAxiosClient();
+
+      // Use the Axios instance to make a POST request to the server, sending the new to-do data
+      const { data } = await axiosInstance.post("http://localhost:8080/todos", newTodo);
+
+      // Return the response data (e.g., the newly created to-do object)
+      return data;
+    },
+  });
+
+  const { mutate: markAsCompleted } = useMutation({
+    mutationKey: ["markAsCompleted"],
+    mutationFn: async (todoId) => {
+     const axiosInstance = await getAxiosClient(); 
+  
+      const { data } = await axiosInstance.put(`http://localhost:8080/todos/${todoId}/completed`);
+  
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries("todos");
+    }
+  });
+
+  const { data, isError, isLoading } = useQuery({
+    // A unique key to identify this query in React Query's cache
+    queryKey: ["todos"],
+
+    // The function responsible for fetching the data
+    queryFn: async () => {
+      const axiosInstance = await getAxiosClient();
+
+      // Use the Axios instance to send a GET request to fetch the list of todos
+      const { data } = await axiosInstance.get("http://localhost:8080/todos");
+
+      // Return the fetched data (React Query will cache it under the queryKey)
+      return data;
+    },
   });
 
   const toggleNewTodoModal = () => {
@@ -39,6 +70,18 @@ export default function Todos() {
       description: ""
     }
   });
+
+  if (isLoading) {
+    return (
+      <div>Loading Todos...</div>
+    )
+  }
+
+  if (isError) {
+    return (
+      <div>There was an error</div>
+    )
+  }
 
   const handleNewTodo = (values) => {
     console.log(values);
@@ -93,6 +136,39 @@ export default function Todos() {
           </form>
         </div>
       </dialog>
+    )
+  }
+
+  function TodoItemList() {
+    return (
+      <div className="w-lg h-sm flex column items-center justify-center gap-4">
+        {data.success && data.todos && data.todos.length >= 1 && (
+          <ul className="flex column items-center justify-center gap-4">
+            {
+              data.todos.map(todo => (
+                <li className="inline-flex items-center gap-4">
+                  <div className="w-md">
+                    <h3 className="text-lg">
+                      {todo.name}
+                    </h3>
+                    <p className="text-sm">{todo.description}</p>
+                  </div>
+                  <div className="w-md">
+                    <label className="swap">
+                      <input type="checkbox" onClick={() => markAsCompleted(todo.id)} />
+                      <div className="swap-on">
+                        Yes
+                      </div>
+                      <div className="swap-off">
+                        No
+                      </div>
+                    </label>
+                  </div>
+                </li>
+              ))
+            }
+          </ul>
+        )}</div>
     )
   }
 
