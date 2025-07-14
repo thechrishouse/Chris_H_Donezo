5b42bc7eca08aa297bae511f7ce88198e73e6bd9
Christopher House <chrishouse180@gmail.com>
2025-07-14 03:43:14 -0700

    prisma schema updated. express server created

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import todoRouter from "./routes/todo.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/todos", todoRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
